9ce47268e7a66bd863642331b2dd5a5be0438165
Ne0 <container@rails.dev>
2018-08-14 01:30:40 +0000

    New: add responses according to task.
    Also fix root route for POST request to get data from JSON correctly (think now will be problems with ordinary form data, need to fix that).
    And change shorten logic, now it generates not unique url and engoding doesn't make sense anymore (need to fix this).
    Remove concatenation with 'http://' string from #clean_url method. I don't like new place for that, need to think and fix.
    Add check if part of url is locale (as I found it important to preserve upcase for locale in some urls). For now just leave locale (actually any part of url which match pattern), but it may allow two equal urls in db.

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'base64'
 require 'yaml'
 require 'active_record'
 require './models/url'
+require 'json'
 
 set :server, 'thin'
 
@@ -14,8 +15,9 @@ ActiveRecord::Base.establish_connection(db_params)
 get '/:url' do
   url = Url.find_by_url(params[:url])
   if url
-    redirect url.longUrl
+    redirect url.longUrl, 301
   else
+    status 404
     "This is not the url you are looking for"
   end
 end
@@ -25,23 +27,36 @@ get '/' do
 end
 
 post '/' do
-  longUrl = clean_url(params[:longUrl])
+  longUrl = clean_url(JSON.parse(request.body.read)["longUrl"])
   shortUrl = shorten_url(longUrl)
-  url = Url.find_or_create_by(url: shortUrl) do |created_url|
-    created_url.longUrl = longUrl
+  url = Url.find_by(longUrl: longUrl)
+  unless url.present?
+  url = Url.create(longUrl: longUrl, url: shortUrl)
   end
-  "#{base_url}/#{url.url}"
+  url = Url.find_or_create_by(longUrl: "http://#{longUrl}") do |created_url|
+    created_url.url = shortUrl
+  end
+  content_type :json
+  { url: "#{base_url}/#{url.url}" }.to_json
 end
 
 def clean_url(raw_url)
+  raw_url = raw_url.to_s if raw_url.nil?
   cleaned_url = raw_url.strip
-  cleaned_url = cleaned_url.downcase.gsub(/(https?:\/\/)|(www\.)/, "")
+  cleaned_url = cleaned_url.gsub(/(https?:\/\/)|(www\.)/, "")
+  cleaned_url = cleaned_url.split('/').map do |urlpart|
+    if /\A([\w]{2})-([\w]{2})\z/.match?(urlpart)
+      urlpart
+    else
+      urlpart.downcase
+    end
+  end.join('/')
   cleaned_url.slice!(-1) if '/' == cleaned_url[-1]
-  'http://' + cleaned_url
+  cleaned_url
 end
 
 def shorten_url(longUrl)
-  Base64.encode64(longUrl)[0..6]
+  Base64.encode64(longUrl).split('').sample(6).join
 end
 
 def base_url
